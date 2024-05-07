6741619e30c6e8c1ecc0f27bec45eddd54288695
Yunir <spire47911@yandex.ru>
2024-05-07 19:27:30 +0500

    Init hotel comit #1

---
+++ SimpleHotel/src/main/java/com/example/simplehotel/SimpleHotelApplication.java
@@ -0,0 +1,13 @@
+package com.example.simplehotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleHotelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleHotelApplication.class, args);
+    }
+
+}
